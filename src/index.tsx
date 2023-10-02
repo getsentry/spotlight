1d4c0103115a1e98562cc5ad48fa57c31143fa66
Daniel Griesser <daniel.griesser.86@gmail.com>
2023-10-02 14:53:37 +0200

    feat: Add sidecar (#3)

--- src/index.tsx
+++ src/index.tsx
@@ -29,6 +29,7 @@ export function init({
 } = {}) {
   if (typeof document === "undefined") return;
 
+  hookIntoSentry();
   connectToRelay(relay);
 
   // build shadow dom container to contain styles
@@ -69,6 +70,43 @@ export function pushEvent(event: SentryEvent) {
 
 export function pushEnvelope(envelope: Envelope) {
   dataCache.pushEnvelope(envelope);
+}
+
+function hookIntoSentry() {
+  // A very hacky way to hook into Sentry's SDK
+  // but we love hacks
+  (window as any).__SENTRY__.hub._stack[0].client.setupIntegrations(true);
+  (window as any).__SENTRY__.hub._stack[0].client.on("beforeEnvelope", (envelope: any) => {
+    fetch('http://localhost:8969/stream', {
+      method: 'POST',
+      body: serializeEnvelope(envelope),
+      headers: {
+        'Content-Type': 'application/x-sentry-envelope',
+      },
+      mode: 'cors',
+    })
+      .catch(err => {
+        console.error(err);
+      });
+  });
+}
+
+function serializeEnvelope(envelope: Envelope): string {
+  const [envHeaders, items] = envelope;
+
+  // Initially we construct our envelope as a string and only convert to binary chunks if we encounter binary data
+  const parts: string[] = [];
+  parts.push(JSON.stringify(envHeaders));
+
+  for (const item of items) {
+    const [itemHeaders, payload] = item;
+
+    parts.push(`\n${JSON.stringify(itemHeaders)}\n`);
+
+    parts.push(JSON.stringify(payload));
+  }
+
+  return parts.join("");
 }
 
 function connectToRelay(relay: string = DEFAULT_RELAY) {
