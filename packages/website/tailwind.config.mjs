9f34d4d281309a24b3e0ff1a421eb60cefd68804
David Cramer <dcramer@users.noreply.github.com>
2023-11-06 03:33:57 -0800

    ref: Convert repository into monorepo (#14)
    Packages:
    
    * spotlight
    * spotlight-sidecar
    * website
    
    ---------
    
    Co-authored-by: Lukas Stracke <lukas.stracke@sentry.io>

---
+++ packages/website/tailwind.config.mjs
@@ -0,0 +1,28 @@
+import starlightPlugin from '@astrojs/starlight-tailwind';
+import typographyPlugin from '@tailwindcss/typography';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      animation: {
+        fadeIn: '0.5s fadeIn forwards',
+        fadeOut: '0.5s fadeOut forwards',
+      },
+
+      // that is actual animation
+      keyframes: () => ({
+        fadeIn: {
+          '0%': { opacity: 0, transform: 'translate(-20px, 0)' },
+          '100%': { opacity: 1, transform: 'translate(0, 0)' },
+        },
+        fadeOut: {
+          '0%': { opacity: 1, transform: 'translate(0, 0)' },
+          '100%': { opacity: 0, transform: 'translate(-20px, 0)' },
+        },
+      }),
+    },
+  },
+  plugins: [starlightPlugin(), typographyPlugin()],
+};
