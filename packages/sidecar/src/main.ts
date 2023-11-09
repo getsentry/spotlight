27086afa210cf1f3a4e4379b9bac550922dbeb0d
Daniel Griesser <daniel.griesser.86@gmail.com>
2023-11-09 15:27:36 +0100

    ref: Nit cleanup

--- packages/sidecar/src/main.ts
+++ packages/sidecar/src/main.ts
@@ -1,4 +1,4 @@
-import { Server, createServer } from "http";
+import { IncomingMessage, Server, ServerResponse, createServer } from 'http';
 
 const defaultResponse = `<!doctype html>
 <html>
@@ -18,68 +18,68 @@ EvtSource.onmessage = function (event) {
 </body>
 </html>`;
 
-function generate_uuidv4() {
+function generateUuidv4(): string {
   let dt = new Date().getTime();
-  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
     let rnd = Math.random() * 16;
     rnd = (dt + rnd) % 16 | 0;
     dt = Math.floor(dt / 16);
-    return (c === "x" ? rnd : (rnd & 0x3) | 0x8).toString(16);
+    return (c === 'x' ? rnd : (rnd & 0x3) | 0x8).toString(16);
   });
 }
 
 class MessageBuffer<T> {
-  private _size: number;
-  private _items: [number, T][];
-  private _writePos: number = 0;
-  private _head: number = 0;
-  private _timeout: number = 10;
-  private _readers: Map<string, (item: T) => void>;
+  private size: number;
+  private items: [number, T][];
+  private writePos = 0;
+  private head = 0;
+  private timeout = 10;
+  private readers = new Map<string, (item: T) => void>();
 
-  public constructor(size = 100) {
-    this._size = size;
-    this._items = new Array(size);
-    this._readers = new Map<string, (item: T) => void>();
+  constructor(size = 100) {
+    this.size = size;
+    this.items = new Array(size);
   }
 
-  public put(item: T): void {
+  put(item: T): void {
     const curTime = new Date().getTime();
-    this._items[this._writePos % this._size] = [curTime, item];
-    this._writePos += 1;
-    if (this._head === this._writePos) {
-      this._head += 1;
+    this.items[this.writePos % this.size] = [curTime, item];
+    this.writePos += 1;
+    if (this.head === this.writePos) {
+      this.head += 1;
     }
 
-    const minTime = curTime - this._timeout * 1000;
+    const minTime = curTime - this.timeout * 1000;
     let atItem;
-    while (this._head < this._writePos) {
-      atItem = this._items[this._head % this._size];
+    while (this.head < this.writePos) {
+      atItem = this.items[this.head % this.size];
       if (atItem === undefined) break;
       if (atItem[0] > minTime) break;
-      this._head += 1;
+      this.head += 1;
     }
   }
 
-  public subscribe(callback: (item: T) => void): string {
-    const readerId = generate_uuidv4();
-    this._readers.set(readerId, callback);
+  subscribe(callback: (item: T) => void): string {
+    const readerId = generateUuidv4();
+    this.readers.set(readerId, callback);
     setTimeout(() => this.stream(readerId));
     return readerId;
   }
 
-  public unsubscribe(readerId: string): void {
-    this._readers.delete(readerId);
+  unsubscribe(readerId: string): void {
+    this.readers.delete(readerId);
   }
 
-  public stream(readerId: string, readPos?: number): void {
-    const cb = this._readers.get(readerId);
+  stream(readerId: string, readPos = this.head): void {
+    const cb = this.readers.get(readerId);
     if (!cb) return;
 
-    let atReadPos = typeof readPos === "undefined" ? this._head : readPos;
+    let atReadPos = readPos;
     let item;
+    /* eslint-disable no-constant-condition */
     while (true) {
-      item = this._items[atReadPos % this._size];
-      if (typeof item === "undefined") {
+      item = this.items[atReadPos % this.size];
+      if (typeof item === 'undefined') {
         break;
       }
       cb(item[1]);
@@ -88,86 +88,88 @@ class MessageBuffer<T> {
     setTimeout(() => this.stream(readerId, atReadPos), 500);
   }
 }
-type Payload = [string, string];
 
-let serverInstance: Server;
+type Payload = [string, string];
 
 function getCorsHeader(): { [name: string]: string } {
   return {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Credentials": "true",
-    "Access-Control-Allow-Headers": "*",
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': 'true',
+    'Access-Control-Allow-Headers': '*',
   };
+}
+
+function handleStreamRequest(req: IncomingMessage, res: ServerResponse, buffer: MessageBuffer<Payload>): void {
+  console.log(`[spotlight] Received request ${req.method} ${req.url}`);
+  if (req.headers.accept && req.headers.accept == 'text/event-stream') {
+    if (req.url == '/stream') {
+      res.writeHead(200, {
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache',
+        ...getCorsHeader(),
+        Connection: 'keep-alive',
+      });
+      res.flushHeaders();
+
+      const sub = buffer.subscribe(([payloadType, data]) => {
+        res.write(`event:${payloadType}\n`);
+        data.split('\n').forEach(line => {
+          res.write(`data:${line}\n`);
+        });
+        res.write('\n');
+      });
+
+      req.on('close', () => {
+        buffer.unsubscribe(sub);
+      });
+    } else {
+      res.writeHead(404);
+      res.end();
+    }
+  } else {
+    if (req.url == '/stream') {
+      if (req.method === 'OPTIONS') {
+        res.writeHead(204, {
+          'Cache-Control': 'no-cache',
+          ...getCorsHeader(),
+        });
+        res.end();
+      } else if (req.method === 'POST') {
+        let body: string = '';
+        req.on('readable', () => {
+          const chunk = req.read();
+          if (chunk !== null) body += chunk;
+        });
+        req.on('end', () => {
+          buffer.put([`${req.headers['content-type']}`, body]);
+          res.writeHead(204, {
+            'Cache-Control': 'no-cache',
+            ...getCorsHeader(),
+            Connection: 'keep-alive',
+          });
+          res.end();
+        });
+      } else {
+        res.writeHead(200, {
+          'Content-Type': 'text/html',
+        });
+        res.write(defaultResponse);
+        res.end();
+      }
+    } else {
+      res.writeHead(404);
+      res.end();
+    }
+  }
 }
 
 function startServer(buffer: MessageBuffer<Payload>, port: number): Server {
   const server = createServer((req, res) => {
-    console.log(`[spotlight] Received request ${req.method} ${req.url}`);
-    if (req.headers.accept && req.headers.accept == "text/event-stream") {
-      if (req.url == "/stream") {
-        res.writeHead(200, {
-          "Content-Type": "text/event-stream",
-          "Cache-Control": "no-cache",
-          ...getCorsHeader(),
-          Connection: "keep-alive",
-        });
-        res.flushHeaders();
-
-        const sub = buffer.subscribe(([payloadType, data]) => {
-          res.write(`event:${payloadType}\n`);
-          data.split("\n").forEach((line) => {
-            res.write(`data:${line}\n`);
-          });
-          res.write("\n");
-        });
-
-        req.on("close", () => {
-          buffer.unsubscribe(sub);
-        });
-      } else {
-        res.writeHead(404);
-        res.end();
-      }
-    } else {
-      if (req.url == "/stream") {
-        if (req.method === "OPTIONS") {
-          res.writeHead(204, {
-            "Cache-Control": "no-cache",
-            ...getCorsHeader(),
-          });
-          res.end();
-        } else if (req.method === "POST") {
-          let body: string = "";
-          req.on("readable", () => {
-            const chunk = req.read();
-            if (chunk !== null) body += chunk;
-          });
-          req.on("end", () => {
-            buffer.put([`${req.headers["content-type"]}`, body]);
-            res.writeHead(204, {
-              "Cache-Control": "no-cache",
-              ...getCorsHeader(),
-              Connection: "keep-alive",
-            });
-            res.end();
-          });
-        } else {
-          res.writeHead(200, {
-            "Content-Type": "text/html",
-          });
-          res.write(defaultResponse);
-          res.end();
-        }
-      } else {
-        res.writeHead(404);
-        res.end();
-      }
-    }
+    handleStreamRequest(req, res, buffer);
   });
 
-  server.on("error", (e) => {
-    if ("code" in e && e.code === "EADDRINUSE") {
-      // console.error('[Spotlight] Address in use, retrying...');
+  server.on('error', e => {
+    if ('code' in e && e.code === 'EADDRINUSE') {
       setTimeout(() => {
         server.close();
         server.listen(port);
@@ -181,6 +183,8 @@ function startServer(buffer: MessageBuffer<Payload>, port: number): Server {
   return server;
 }
 
+let serverInstance: Server;
+
 export function setupSidecar(): void {
   const buffer: MessageBuffer<Payload> = new MessageBuffer<Payload>();
 
@@ -191,11 +195,11 @@ export function setupSidecar(): void {
 
 function shutdown() {
   if (serverInstance) {
-    console.log("[Spotlight] Shutting down server");
+    console.log('[Spotlight] Shutting down server');
     serverInstance.close();
   }
 }
 
-process.on("SIGTERM", () => {
+process.on('SIGTERM', () => {
   shutdown();
 });
