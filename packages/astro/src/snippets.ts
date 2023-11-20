5c2ed3cefd9384031eac995cd5dbc84929e0b618
Lukas Stracke <lukas.stracke@sentry.io>
2023-11-20 12:25:17 +0100

    doc

--- packages/astro/src/snippets.ts
+++ packages/astro/src/snippets.ts
@@ -33,6 +33,11 @@ ${buildClientInit(options)}
 /**
  * Hook into Vite's client code to enable Spotlight if an error occurs.
  *
+ * Main difference to normal page injection: We only initialize spotlight
+ * if an error happens and is transmitted via the websocket. This is to avoid
+ * initializing this spotlight instance before the actual app's instance.
+ * This should only be  the fallback instance if the app failed to intialize.
+ *
  * Used variables from Vite's client code:
  * - `enableOverlay` to check if the overlay should be shown
  *    @see https://github.com/vitejs/vite/blob/b9ee620108819e06023e4303af75a61d3e4e4d76/packages/vite/src/client/client.ts#L289
