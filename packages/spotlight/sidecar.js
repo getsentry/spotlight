0a4b4fc41aa91f261d7c7734ed3ce158293875a0
Daniel Griesser <daniel.griesser.86@gmail.com>
2023-11-29 11:32:58 +0100

    feat: Add spotlight package

---
+++ packages/spotlight/sidecar.js
@@ -0,0 +1,5 @@
+Object.defineProperty(exports, '__esModule', { value: true });
+
+const sidecar = require('@spotlightjs/sidecar');
+
+exports.setupSidecar = sidecar.setupSidecar;
---
+++ packages/spotlight/spotlight.js
@@ -0,0 +1,5 @@
+Object.defineProperty(exports, '__esModule', { value: true });
+
+const spotlight = require('@spotlightjs/overlay');
+
+exports.Spotlight = spotlight;
