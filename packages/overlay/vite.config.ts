58a56d2bf5cac697d598312a617f203b2e06a20e
Daniel Griesser <daniel.griesser.86@gmail.com>
2023-12-02 10:36:23 +0100

    Config changes

--- packages/overlay/vite.config.ts
+++ packages/overlay/vite.config.ts
@@ -26,6 +26,7 @@ export default defineConfig({
       // the proper extensions will be added
       fileName: 'sentry-spotlight',
     },
+    sourcemap: true,
     // rollupOptions: {
     //   // make sure to externalize deps that shouldn't be bundled
     //   // into your library
