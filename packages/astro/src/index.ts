e2c3c90e955269e25469f8e4b1db9456642da5c2
Lukas Stracke <lukas.stracke@sentry.io>
2023-12-04 17:19:09 +0100

    ref(astro): Check for Astro 4 `config.devToolbar` in Spotlight integration (#212)

--- packages/astro/src/index.ts
+++ packages/astro/src/index.ts
@@ -1,4 +1,4 @@
-import type { AstroIntegration } from 'astro';
+import type { AstroConfig, AstroIntegration } from 'astro';
 import { buildClientInitSnippet, buildServerSnippet } from './snippets';
 
 import path from 'path';
@@ -9,6 +9,15 @@ import { errorPageInjectionPlugin } from './vite/error-page';
 
 const PKG_NAME = '@spotlightjs/astro';
 
+type AstroConfigWithExperimentalDevOverlay = AstroConfig & {
+  experimental?: {
+    /**
+     * This used to be the way of enabling the dev overlay pre Astro 4.x
+     */
+    devOverlay?: boolean;
+  };
+};
+
 const createPlugin = (options?: SpotlightAstroIntegrationOptions): AstroIntegration => {
   const thisFilePath = url.fileURLToPath(import.meta.url);
 
@@ -16,7 +25,14 @@ const createPlugin = (options?: SpotlightAstroIntegrationOptions): AstroIntegrat
     name: PKG_NAME,
 
     hooks: {
-      'astro:config:setup': async ({ command, injectScript, addDevOverlayPlugin, logger, config }) => {
+      'astro:config:setup': async ({
+        command,
+        injectScript,
+        addDevOverlayPlugin,
+        addDevToolbarApp,
+        logger,
+        config,
+      }) => {
         if (command === 'dev') {
           logger.info('[@spotlightjs/astro] Setting up Spotlight');
 
@@ -30,26 +46,33 @@ const createPlugin = (options?: SpotlightAstroIntegrationOptions): AstroIntegrat
             ...(config.vite.plugins || []),
           ];
 
-          let showTriggerButton = true;
-          // We suppose with 4.0 this will be promoted top level and expimental will be removed
-          // to keep backwards compatibility we check both
-          // @ts-ignore
-          if (config.devOverlay) {
-            // @ts-ignore
-            showTriggerButton = config.devOverlay ? false : true;
-          }
-          // @ts-ignore
-          if (config.experimental?.devOverlay) {
-            // @ts-ignore
-            showTriggerButton = config.experimental?.devOverlay ? false : true;
-          }
+          // Since Astro 4.0.0-beta.4, `devToolbar` is set and enabled by default.
+          // briefly, `devOverlay` was also added to the config but is now deprecated.
+          // Setting either of these to `true` or not setting any of them in the config file
+          // will lead to both of them being enabled by the time this hook is called.
+          // Setting one of them to `false` will not set the other one to false.
+          // Therefore, both of them have to be `true` that we know that the toolbar is in fact active.
+          const hasToolbarEnabled = config.devToolbar.enabled && config.devOverlay.enabled;
+
+          // Before Astro 4, `devOverlay` was disabled by default and under `experimental`
+          const hasExperimentalDevOverlayEnabled = !!(config as AstroConfigWithExperimentalDevOverlay).experimental
+            ?.devOverlay;
+
+          const showTriggerButton = !hasToolbarEnabled && !hasExperimentalDevOverlayEnabled;
 
           injectScript('page', buildClientInitSnippet({ importPath: PKG_NAME, showTriggerButton, ...options }));
           injectScript('page-ssr', buildServerSnippet(options));
 
           const importPath = path.dirname(url.fileURLToPath(import.meta.url));
           const pluginPath = path.join(importPath, 'overlay/index.ts');
-          addDevOverlayPlugin(pluginPath);
+
+          // `addDevToolbarApp` was added in Astro 4.0.0-beta.4.
+          if (typeof addDevToolbarApp === 'function') {
+            addDevToolbarApp(pluginPath);
+          } else {
+            // fall back to old name for pre 4.x support
+            addDevOverlayPlugin(pluginPath);
+          }
         } else if (options?.__debugOptions) {
           injectScript('page', buildClientInitSnippet({ importPath: PKG_NAME, ...options }));
         }
