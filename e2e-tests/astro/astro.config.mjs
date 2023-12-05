34d0f69f57c9de7a13b2a052a9cccb584480e63b
Lukas Stracke <lukas.stracke@sentry.io>
2023-12-05 16:42:39 +0100

    add node adapter

--- e2e-tests/astro/astro.config.mjs
+++ e2e-tests/astro/astro.config.mjs
@@ -3,7 +3,11 @@ import { defineConfig } from 'astro/config';
 import sentry from '@sentry/astro';
 import spotlightjs from '@spotlightjs/astro';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [sentry(), spotlightjs()],
+  adapter: node({ mode: 'standalone' }),
+  output: 'server',
 });
