ca8e07a92ae2d12b20da861e41fe7d296865c709
Lukas Stracke <lukas.stracke@sentry.io>
2023-12-06 10:39:30 +0100

    wip

--- e2e-tests/astro/tests/spotlightAstro.spec.ts
+++ e2e-tests/astro/tests/spotlightAstro.spec.ts
@@ -1,6 +1,8 @@
 import { expect, test } from '@playwright/test';
 import { waitForAstroToolbar } from './utils/test-utils';
 
+const ASTRO_TOOLBAR_SPOTLIGHT_BTN = 'button[data-plugin-id="spotlight-plugin"]';
+
 test('has Astro Dev Toolbar and Spotlight toolbar app', async ({ page }) => {
   await page.goto('http://localhost:4321');
 
@@ -16,19 +18,24 @@ test('Astro dev toolbar has spotlight button', async ({ page }) => {
   await page.goto('http://localhost:4321');
   await waitForAstroToolbar(page);
 
-  const spotlightToolbarButtonVisible = await page.isVisible('button[data-plugin-id="spotlight-plugin"]');
+  const spotlightToolbarButtonVisible = await page.isVisible(ASTRO_TOOLBAR_SPOTLIGHT_BTN);
   expect(spotlightToolbarButtonVisible).toBe(true);
 
   const spotlightTriggerButtonVisible = await page.isVisible('div[title="Spotlight by Sentry"]');
   expect(spotlightTriggerButtonVisible).toBe(false);
 });
 
-test('clicking on spotlight button opens spotlight', async ({ page }) => {
+test('clicking on spotlight button opens and closes spotlight', async ({ page }) => {
   await page.goto('http://localhost:4321');
   await waitForAstroToolbar(page);
 
-  await page.getByRole('button', {});
+  await page.click(ASTRO_TOOLBAR_SPOTLIGHT_BTN);
 
-  const spotlightTriggerButtonVisible = await page.isVisible('div[title="Spotlight by Sentry"]');
-  expect(spotlightTriggerButtonVisible).toBe(true);
+  await page.waitForTimeout(1000);
+  expect(await page.getByText('Spotlight by Sentry').isVisible()).toBe(true);
+
+  await page.click(ASTRO_TOOLBAR_SPOTLIGHT_BTN);
+  await page.waitForTimeout(1000);
+
+  expect(await page.getByText('Spotlight by Sentry').isVisible()).toBe(false);
 });
