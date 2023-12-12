eef528a2b123b66fc299ff90badf147ddb53a2fc
Bruno Garcia <bruno@brunogarcia.com>
2023-12-12 17:19:39 -0500

    dotnet logo (#265)

--- packages/overlay/src/integrations/sentry/components/PlatformIcon.tsx
+++ packages/overlay/src/integrations/sentry/components/PlatformIcon.tsx
@@ -1,6 +1,8 @@
 import { ReactComponent as AstroIcon } from 'platformicons/svg/astro.svg';
 import { ReactComponent as DefaultIcon } from 'platformicons/svg/default.svg';
+import { ReactComponent as DotNetIcon } from 'platformicons/svg/dotnet.svg';
 import { ReactComponent as JavaScriptIcon } from 'platformicons/svg/javascript.svg';
+import { ReactComponent as DotNetMauiIcon } from 'platformicons/svg/maui.svg';
 import { ReactComponent as NodeIcon } from 'platformicons/svg/nodejs.svg';
 import { ReactComponent as PhpIcon } from 'platformicons/svg/php.svg';
 import { ReactComponent as PythonIcon } from 'platformicons/svg/python.svg';
@@ -10,7 +12,7 @@ import { SentryEvent } from '../types';
 
 import { ComponentPropsWithoutRef } from 'react';
 
-type Platform = 'python' | 'javascript' | 'node' | 'ruby' | string;
+type Platform = 'python' | 'javascript' | 'node' | 'ruby' | 'csharp' | string;
 
 export default function PlatformIcon({
   platform,
@@ -36,6 +38,11 @@ export default function PlatformIcon({
       return <NodeIcon width={size} height={size} {...props} />;
     case 'php':
       return <PhpIcon width={size} height={size} {...props} />;
+    case 'dotnet':
+    case 'csharp': // event.platform is 'csharp'
+      return <DotNetIcon width={size} height={size} {...props} />;
+    case 'dotnet.maui':
+      return <DotNetMauiIcon width={size} height={size} {...props} />;
     default:
       return <DefaultIcon width={size} height={size} {...props} />;
   }
