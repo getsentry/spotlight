f15057830d100458fb6d35fd86b441ff65a78118
Shubhdeep Chhabra <chhabrashubhdeep@gmail.com>
2024-06-05 10:09:24 +0530

    updated renderValue util

--- packages/overlay/src/utils/values.tsx
+++ packages/overlay/src/utils/values.tsx
@@ -1,37 +1,21 @@
 export function renderValue(value: unknown): string {
-  if (value === undefined || value === null) {
-    return String(value);
-  } else if (
-    typeof value === 'boolean' ||
-    typeof value === 'number' ||
-    typeof value === 'bigint' ||
-    typeof value === 'string' ||
-    typeof value === 'symbol' ||
-    typeof value === 'function' ||
-    value instanceof Date
-  ) {
-    return String(value);
-  } else if (Array.isArray(value)) {
-    return '[' + value.map(renderValue).join(', ') + ']';
-  } else if (value instanceof Map) {
-    return (
-      '{' +
-      Array.from(value.entries())
-        .map(([key, val]) => `${renderValue(key)}: ${renderValue(val)}`)
-        .join(', ') +
-      '}'
-    );
-  } else if (value instanceof Set) {
-    return '{' + Array.from(value).map(renderValue).join(', ') + '}';
-  } else if (typeof value === 'object') {
-    return (
-      '{' +
-      Object.entries(value)
-        .map(([key, val]) => `${key}: ${renderValue(val)}`)
-        .join(', ') +
-      '}'
-    );
-  } else {
-    return String(value);
+  if (value === undefined) {
+    return 'undefined';
   }
+  return JSON.stringify(
+    value,
+    (_key, val) => {
+      if (typeof val === 'function') {
+        return val.toString();
+      }
+      if (typeof val === 'symbol') {
+        return val.toString();
+      }
+      if (typeof val === 'bigint') {
+        return val.toString();
+      }
+      return val;
+    },
+    2,
+  );
 }
