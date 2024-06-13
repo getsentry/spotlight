076d953f86dd409f2a4b3d4aa07c6b1c2b6f58e5
Shubhdeep Chhabra <chhabrashubhdeep@gmail.com>
2024-06-13 16:52:48 +0530

    fix: Fix error handling in electron app and rendering of frame variables in stacktrace on Error Event detail page (#408)
    * - fix(electron): Fixed error handling in electron app.
    - fix(overlay): Fixed parsing of frame vars present in stacktrace in Error Events.
    
    * stopped spotlight debug for electron
    
    * updated renderValue util

---
+++ packages/overlay/src/utils/values.tsx
@@ -0,0 +1,21 @@
+export function renderValue(value: unknown): string {
+  if (value === undefined) {
+    return 'undefined';
+  }
+  return JSON.stringify(
+    value,
+    (_key, val) => {
+      if (typeof val === 'function') {
+        return val.toString();
+      }
+      if (typeof val === 'symbol') {
+        return val.toString();
+      }
+      if (typeof val === 'bigint') {
+        return val.toString();
+      }
+      return val;
+    },
+    2,
+  );
+}
