27541cd19377d4685005c20f5e69cf1a2a08cbdf
Burak Yigit Kaya <byk@sentry.io>
2024-07-22 22:15:41 +0100

    revert obsolete change

--- packages/overlay/test/integrations/sentry/data/sentryDataCache.test.ts
+++ packages/overlay/test/integrations/sentry/data/sentryDataCache.test.ts
@@ -6,7 +6,7 @@ import fs from 'fs';
 
 describe('SentryDataCache', () => {
   // We need to refactor this to make it actually testable
-  test('Process Envelope', async () => {
+  test('Process Envelope', () => {
     const envelope = fs.readFileSync('./_fixtures/envelope_javascript.txt', 'utf-8');
     const processedEnvelope = processEnvelope({ data: envelope, contentType: 'test' });
     expect(
