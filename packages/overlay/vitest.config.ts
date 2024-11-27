2e67d24470397285bb13668af4e9f6013973cd2e
Burak Yigit Kaya <byk@sentry.io>
2024-11-27 19:08:56 +0000

    ci: Overhaul build & publish process to be more efficient (#580)

--- packages/overlay/vitest.config.ts
+++ packages/overlay/vitest.config.ts
@@ -4,11 +4,15 @@
 import { resolve } from 'node:path';
 import { defineConfig } from 'vitest/config';
 
+const isCI = Boolean(process.env.CI);
 export default defineConfig({
   //   plugins: [tsconfigPaths()],
   test: {
     environment: 'happy-dom',
+    reporters: isCI ? ['junit', 'default'] : ['default'],
+    outputFile: 'junit.xml',
     coverage: {
+      enabled: isCI,
       provider: 'v8',
       reporter: ['json'],
     },
