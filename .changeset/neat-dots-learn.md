9a56f7dac9f2225694152aa6e583d510b142f431
Burak Yigit Kaya <byk@sentry.io>
2024-12-02 22:57:46 +0000

    feat: Force-enable tracing with spotlight injection

---
+++ .changeset/neat-dots-learn.md
@@ -0,0 +1,5 @@
+---
+'@spotlightjs/overlay': patch
+---
+
+Force-enable tracing with FE spotlight injection
--- demos/astro-playground/sentry.client.config.ts
+++ demos/astro-playground/sentry.client.config.ts
@@ -2,6 +2,4 @@ import * as Sentry from '@sentry/astro';
 
 Sentry.init({
   debug: true,
-  integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
-  tracesSampleRate: 1.0,
 });
