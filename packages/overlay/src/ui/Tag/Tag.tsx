057b3a34addbcff1b6124c964a44a5a1d9be63f8
Shubhdeep Chhabra <chhabrashubhdeep@gmail.com>
2024-12-03 10:12:51 +0530

    minor tweak in tag component

--- packages/overlay/src/ui/Tag/Tag.tsx
+++ packages/overlay/src/ui/Tag/Tag.tsx
@@ -1,14 +1,10 @@
-import classNames from '~/lib/classNames';
-
 export type TagProps = { tagKey: string; value: string };
 
 export default function Tag({ tagKey, value }: TagProps) {
-  return (
-    <div className="border-primary-300 bg-primary-900 divide-x-primary-300 inline-flex divide-x whitespace-nowrap rounded-full border font-mono text-sm">
+  return value ? (
+    <div className="border-primary-300 bg-primary-900 divide-x-primary-300 inline-flex divide-x overflow-hidden whitespace-nowrap rounded-full border font-mono text-sm">
       {tagKey ? <div className="px-2 py-1 font-semibold">{tagKey}</div> : null}
-      <div className={classNames('bg-primary-800', 'px-2', 'py-1', tagKey ? 'rounded-r-full' : 'rounded-full')}>
-        {value}
-      </div>
+      <div className="bg-primary-800 rounded-full px-2 py-1">{value}</div>
     </div>
-  );
+  ) : null;
 }
