9606eb25434468bb9377569667f4071a177b85ab
Burak Yigit Kaya <byk@sentry.io>
2025-01-17 22:14:05 +0000

    dev: Add hot-reload based dev flow (#657)
    A little bit copy/pasta and janky but is much better than waiting for
    the full rebuild cycles + manual refreshes.
    
    This should unblock us and we can then iterate on this a bit for real
    HMR etc.

---
+++ .changeset/giant-bugs-suffer.md
@@ -0,0 +1,5 @@
+---
+'@spotlightjs/overlay': patch
+---
+
+Add auto-reload dev mode for Overlay
---
+++ packages/overlay/vite.dev.config.ts
@@ -0,0 +1,48 @@
+import spotlightSidecar from '@spotlightjs/sidecar/vite-plugin';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+import svgr from 'vite-plugin-svgr';
+
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [
+    spotlightSidecar(),
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+    svgr({
+      svgrOptions: {
+        titleProp: true,
+      },
+    }),
+  ],
+  define: {
+    'process.env.NODE_ENV': '"development"',
+    'process.env.npm_package_version': JSON.stringify(process.env.npm_package_version),
+  },
+  resolve: {
+    alias: {
+      '~': resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    outDir: resolve(__dirname, 'dist', 'overlay'),
+    manifest: 'manifest.json',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+      // We disable versioned filenames here explicitly
+      // so we can include the script when sidecar is running
+      // for server-side frameworks
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]',
+      },
+    },
+  },
+});
