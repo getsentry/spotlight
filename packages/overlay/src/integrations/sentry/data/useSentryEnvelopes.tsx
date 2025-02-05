4cde1351151520d576fa173485993f30a29a594a
Burak Yigit Kaya <byk@sentry.io>
2025-02-05 23:12:27 +0000

    fix: Sort envelopes in recent to old (#697)
    Fixes #696.

--- packages/overlay/src/integrations/sentry/data/useSentryEnvelopes.tsx
+++ packages/overlay/src/integrations/sentry/data/useSentryEnvelopes.tsx
@@ -6,7 +6,13 @@ import { useSentryHelpers } from './useSentryHelpers';
 export const useSentryEnvelopes = () => {
   useContext(SentryEventsContext);
   const helpers = useSentryHelpers();
-  const allEnvelopes = sentryDataCache.getEnvelopes();
+  const allEnvelopes = sentryDataCache.getEnvelopes().sort((a, b) => {
+    const a_sent_at = a.envelope[0].sent_at as string;
+    const b_sent_at = b.envelope[0].sent_at as string;
+    if (a_sent_at < b_sent_at) return 1;
+    if (a_sent_at > b_sent_at) return 1;
+    return 0;
+  });
 
   const localEnvelopes = allEnvelopes.filter(({ envelope }) => {
     const { trace_id } = (envelope[0]?.trace as { trace_id?: string }) || {};
