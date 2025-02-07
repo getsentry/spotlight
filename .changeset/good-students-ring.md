05e03c749892af0042f3cc4e3e15b85fd5dcdaac
Burak Yigit Kaya <byk@sentry.io>
2025-02-07 12:17:40 +0000

    fix: Fix span details modal resetting tree on close (#701)

---
+++ .changeset/good-students-ring.md
@@ -0,0 +1,8 @@
+---
+'@spotlightjs/spotlight': patch
+'@spotlightjs/electron': patch
+'@spotlightjs/overlay': patch
+'@spotlightjs/astro': patch
+---
+
+Fix span details modal resetting trace view tree state on close
--- packages/overlay/src/ui/SidePanel/SidePanel.tsx
+++ packages/overlay/src/ui/SidePanel/SidePanel.tsx
@@ -1,9 +1,17 @@
 import type { ComponentPropsWithoutRef, ReactNode } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 export type SidePanelProps = Omit<ComponentPropsWithoutRef<'div'>, 'className'> & {
   backto: string;
 };
+
+// This trick is taken from:
+// https://github.com/remix-run/react-router/discussions/9922#discussioncomment-4722716
+export function useGoBackWithFallback(fallback: string) {
+  const navigateTo = useNavigate();
+  const loc = useLocation();
+  return () => (loc.key === 'default' ? navigateTo(-1) : navigateTo(fallback, { replace: true }));
+}
 
 export function SidePanelHeader({
   title,
@@ -14,29 +22,28 @@ export function SidePanelHeader({
   subtitle?: ReactNode;
   backto: string;
 }) {
+  const goBackWithFallback = useGoBackWithFallback(backto);
+
   return (
     <div className="border-b-primary-400 mb-4 flex border-b pb-4">
       <div className="flex-1">
         <h2 className="text-primary-300 text-xl">{title}</h2>
         {subtitle && <h3 className="font-mono">{subtitle}</h3>}
       </div>
-      <Link
+      <button
         className="hover:bg-primary-900 -my-1 flex cursor-pointer items-center justify-center rounded px-6 py-1 font-mono text-2xl"
-        to={backto}
+        onClick={goBackWithFallback}
       >
         {'✕'}
-      </Link>
+      </button>
     </div>
   );
 }
 
 export default function SidePanel(props: SidePanelProps) {
-  const navigateTo = useNavigate();
+  const goBackWithFallback = useGoBackWithFallback(props.backto);
   return (
-    <div
-      className="fixed bottom-0 left-0 right-0 top-0 z-10 bg-black bg-opacity-30"
-      onClick={() => navigateTo(props.backto)}
-    >
+    <div className="fixed bottom-0 left-0 right-0 top-0 z-10 bg-black bg-opacity-30" onClick={goBackWithFallback}>
       <div
         onClick={e => e.stopPropagation()}
         className="spotlight-sidepanel border-l-primary-400 from-primary-900 to-primary-950 fixed bottom-0 left-1/4 right-0 top-0 z-20 flex h-full flex-col overflow-auto border-l bg-gradient-to-br to-20% px-6 py-4"
