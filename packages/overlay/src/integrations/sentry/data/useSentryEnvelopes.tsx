f18db7359518aae22cc1d9a18870946af0f0c9d6
Burak Yigit Kaya <byk@sentry.io>
2025-03-05 18:16:42 +0000

    fix: Fix envelope sorting and local classification

--- packages/overlay/src/integrations/sentry/data/useSentryEnvelopes.tsx
+++ packages/overlay/src/integrations/sentry/data/useSentryEnvelopes.tsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import type { Trace } from '../types';
 import sentryDataCache from './sentryDataCache';
 import { SentryEventsContext } from './sentryEventsContext';
 import { useSentryHelpers } from './useSentryHelpers';
@@ -10,14 +11,14 @@ export const useSentryEnvelopes = () => {
     const a_sent_at = a.envelope[0].sent_at as string;
     const b_sent_at = b.envelope[0].sent_at as string;
     if (a_sent_at < b_sent_at) return 1;
-    if (a_sent_at > b_sent_at) return 1;
+    if (a_sent_at > b_sent_at) return -1;
     return 0;
   });
 
   const localEnvelopes = allEnvelopes.filter(({ envelope }) => {
-    const { trace_id } = (envelope[0]?.trace as { trace_id?: string }) || {};
+    const trace_id = (envelope[0]?.trace as Trace)?.trace_id;
     if (trace_id) return helpers.isLocalToSession(trace_id) !== false;
-    return false;
+    return true;
   });
   return { allEnvelopes, localEnvelopes };
 };
