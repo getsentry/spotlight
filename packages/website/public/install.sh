c8a34f4ba441f1a1102463825fb80e7542273d56
Burak Yigit Kaya <byk@sentry.io>
2025-04-22 20:20:22 +0100

    fix(install): Fix install script on non-Linux (#782)
    Fixes #781.

--- packages/website/public/install.sh
+++ packages/website/public/install.sh
@@ -1,12 +1,12 @@
 os=$(uname -s | tr '[:upper:]' '[:lower:]' | cut -c -7)
-arch=$(uname -i | tr '[:upper:]' '[:lower:]' | cut -c -7)
+arch=$(uname -m | tr '[:upper:]' '[:lower:]' | cut -c -7)
 suffix=''
 
 if [ "$os" = "linux" ]; then
   os='linux'
 elif [ "$os" = "darwin" ]; then
   os='darwin'
-elif [ "$os" = "msys_nt"]; then
+elif [ "$os" = "msys_nt" ]; then
   os='win'
   suffix='.exe'
 else
@@ -26,8 +26,11 @@ else
 fi
 
 spotlight_path='~/.local/bin/spotlight'
+spotlight_file="spotlight-$os-$arch$suffix"
 mkdir -p $(dirname $spotlight_path)
-curl -qfL "https://github.com/getsentry/spotlight/releases/latest/download/spotlight-$os-$arch$suffix" -o $spotlight_path
+echo "Downloading $spotlight_file..."
+curl --progress-bar -fL "https://github.com/getsentry/spotlight/releases/latest/download/$spotlight_file" -o $spotlight_path
 chmod +x $spotlight_path
 echo "Spotlight installed to $spotlight_path"
+echo "Run 'rm $spotlight_path' to uninstall"
 $spotlight_path
