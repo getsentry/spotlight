b8168ad06a1842359373103e44cbb1ce5b0e8c3e
David Cramer <dcramer@gmail.com>
2025-06-02 13:49:25 -0700

    Upgrade to tailwind 4

--- packages/overlay/src/ui/sidePanel.tsx
+++ packages/overlay/src/ui/sidePanel.tsx
@@ -1,7 +1,7 @@
-import type { ComponentPropsWithoutRef, ReactNode } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import type { ComponentPropsWithoutRef, ReactNode } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
-export type SidePanelProps = Omit<ComponentPropsWithoutRef<'div'>, 'className'> & {
+export type SidePanelProps = Omit<ComponentPropsWithoutRef<"div">, "className"> & {
   backto: string;
 };
 
@@ -10,7 +10,7 @@ export type SidePanelProps = Omit<ComponentPropsWithoutRef<'div'>, 'className'> 
 export function useGoBackWithFallback(fallback: string) {
   const navigateTo = useNavigate();
   const loc = useLocation();
-  return () => (loc.key === 'default' ? navigateTo(fallback, { replace: true }) : navigateTo(-1));
+  return () => (loc.key === "default" ? navigateTo(fallback, { replace: true }) : navigateTo(-1));
 }
 
 export function SidePanelHeader({
@@ -35,7 +35,7 @@ export function SidePanelHeader({
         className="hover:bg-primary-900 -my-1 flex cursor-pointer items-center justify-center rounded px-6 py-1 font-mono text-2xl"
         onClick={goBackWithFallback}
       >
-        {'✕'}
+        {"✕"}
       </button>
     </div>
   );
@@ -46,8 +46,8 @@ export default function SidePanel(props: SidePanelProps) {
   return (
     <div className="fixed bottom-0 left-0 right-0 top-0 z-10 bg-black bg-opacity-30" onClick={goBackWithFallback}>
       <div
-        onClick={e => e.stopPropagation()}
-        className="spotlight-sidepanel border-l-primary-400 from-primary-900 to-primary-950 fixed bottom-0 left-1/4 right-0 top-0 z-20 flex h-full flex-col overflow-auto border-l bg-gradient-to-br to-20% px-6 py-4"
+        onClick={(e) => e.stopPropagation()}
+        className="spotlight-sidepanel border-l-primary-400 from-primary-900 to-primary-950 fixed bottom-0 left-1/4 right-0 top-0 z-20 flex h-full flex-col overflow-auto border-l bg-linear-to-br to-20% px-6 py-4"
         {...props}
       />
     </div>
