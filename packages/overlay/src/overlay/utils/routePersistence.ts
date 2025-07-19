4947e4abf27ee01d1649003749bee769b5f4cf1d
Shubhdeep Chhabra <chhabrashubhdeep@gmail.com>
2025-07-19 13:41:31 +0530

    spell checks

--- packages/overlay/src/overlay/utils/routePersistence.ts
+++ packages/overlay/src/overlay/utils/routePersistence.ts
@@ -1,5 +1,5 @@
 /**
- * teturns a namespaced sessionStorage key for storing the last route.
+ * returns a namespaced sessionStorage key for storing the last route.
  * @param contextId unique identifier for the current context (e.g., sidecarUrl or window.location.origin)
  */
 export function getRouteStorageKey(contextId: string) {
