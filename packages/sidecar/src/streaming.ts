ec012c0fe60303880810f667c48b846318c20dfd
Aditya Mathur <57684218+MathurAditya724@users.noreply.github.com>
2025-08-12 14:48:17 -0700

    ref(sidecar): Migrate the server logic to Hono (#862)
    - Shifted to honojs

---
+++ packages/sidecar/src/streaming.ts
@@ -0,0 +1,41 @@
+import type { Context } from "hono";
+import { SSEStreamingApi } from "hono/streaming";
+
+const run = async (
+  stream: SSEStreamingApi,
+  cb: (stream: SSEStreamingApi) => Promise<void>,
+  onError?: (e: Error, stream: SSEStreamingApi) => Promise<void>,
+): Promise<void> => {
+  try {
+    await cb(stream);
+  } catch (e) {
+    if (e instanceof Error && onError) {
+      await onError(e, stream);
+
+      await stream.writeSSE({
+        event: "error",
+        data: e.message,
+      });
+    } else {
+      console.error(e);
+    }
+  }
+};
+
+export const streamSSE = (
+  c: Context,
+  cb: (stream: SSEStreamingApi) => Promise<void>,
+  onError?: (e: Error, stream: SSEStreamingApi) => Promise<void>,
+): Response => {
+  const { readable, writable } = new TransformStream();
+  const stream = new SSEStreamingApi(writable, readable);
+
+  c.header("Transfer-Encoding", "chunked");
+  c.header("Content-Type", "text/event-stream");
+  c.header("Cache-Control", "no-cache");
+  c.header("Connection", "keep-alive");
+
+  run(stream, cb, onError);
+
+  return c.newResponse(stream.responseReadable);
+};
