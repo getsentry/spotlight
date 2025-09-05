18b2e24bfc252201fdd6579669cf2af0a1d9d2db
betegon <miguelbetegongarcia@gmail.com>
2025-09-05 14:14:50 +0200

    remove overlay plugin

--- packages/overlay/vite.config.ts
+++ packages/overlay/vite.config.ts
@@ -2,26 +2,10 @@ import { resolve, sep } from "node:path";
 import spotlightSidecar from "@spotlightjs/sidecar/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import MagicString from "magic-string";
 import type { RenderedChunk } from "rollup";
-import type { PluginOption } from "vite";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 import svgr from "vite-plugin-svgr";
-
-const removeReactDevToolsMessagePlugin: () => PluginOption = () => ({
-  name: "remove-react-devtools-message",
-  transform(code, id) {
-    if (id.includes(`${sep}react-dom${sep}`) && code.includes("__REACT_DEVTOOLS_GLOBAL_HOOK__")) {
-      const ms = new MagicString(code);
-      ms.replaceAll("__REACT_DEVTOOLS_GLOBAL_HOOK__", "({ isDisabled: true })");
-      return {
-        code: ms.toString(),
-        map: ms.generateMap({ hires: true }),
-      };
-    }
-  },
-});
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -36,7 +20,6 @@ export default defineConfig({
         titleProp: true,
       },
     }),
-    removeReactDevToolsMessagePlugin(),
     tailwindcss(),
   ],
   define: {
