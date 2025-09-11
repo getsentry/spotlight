e381b17afde68e1d841e427ae9ed3353e8fee9fd
mathuraditya724 <mathuraditya724@gmail.com>
2025-09-11 14:41:10 +0530

    chore: minor changes

--- packages/electron/src/index.ts
+++ packages/electron/src/index.ts
@@ -1,29 +1,25 @@
 import * as Sentry from "@sentry/electron/renderer";
 import * as Spotlight from "@spotlightjs/overlay";
 
-Sentry.init({
-  dsn: "https://192df1a78878de014eb416a99ff70269@o1.ingest.sentry.io/4506400311934976",
-  environment: process.env.NODE_ENV,
-  release: `spotlight@${process.env.npm_package_version}`,
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration({
-      // Additional SDK configuration goes in here, for example:
-      maskAllText: true,
-      blockAllMedia: true,
-    }),
-  ],
-  tracesSampleRate: 1.0,
-  profilesSampleRate: 1.0,
-  replaysSessionSampleRate: 1.1,
-  replaysOnErrorSampleRate: 1.0,
-});
+// Sentry.init({
+//   dsn: "https://192df1a78878de014eb416a99ff70269@o1.ingest.sentry.io/4506400311934976",
+//   environment: process.env.NODE_ENV,
+//   release: `spotlight@${process.env.npm_package_version}`,
+//   integrations: [
+//     Sentry.browserTracingIntegration(),
+//     Sentry.replayIntegration({
+//       // Additional SDK configuration goes in here, for example:
+//       maskAllText: true,
+//       blockAllMedia: true,
+//     }),
+//   ],
+//   tracesSampleRate: 1.0,
+//   profilesSampleRate: 1.0,
+//   replaysSessionSampleRate: 1.1,
+//   replaysOnErrorSampleRate: 1.0,
+// });
 
 Spotlight.init({
-  fullPage: true,
-  injectImmediately: true,
-  showTriggerButton: false,
-  integrations: [Spotlight.sentry({ injectIntoSDK: false })],
   showClearEventsButton: false,
 });
 
