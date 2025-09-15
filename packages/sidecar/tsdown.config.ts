942a87d72af6f16fa3164735ffa53b837a40c7cc
mathuraditya724 <mathuraditya724@gmail.com>
2025-09-15 12:26:32 +0530

    chore: minor change

--- packages/sidecar/tsdown.config.ts
+++ packages/sidecar/tsdown.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "tsdown";
-import pkg from "./package.json" assert { type: "json" };
 
 export default defineConfig({
-  entry: ["./server.ts", ...Object.values(pkg.exports).map(x => x.import.replace("dist/", "").replace(".js", ".ts"))],
+  entry: [
+    "./src/main.ts",
+    "./src/vite-plugin.ts",
+    "./src/constants.ts",
+    "./server.ts",
+    "./src/parser/index.ts",
+    "./src/format/index.ts",
+  ],
   sourcemap: true,
 });
