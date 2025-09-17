8bc91a170a1f0e94b1e5a0d44b95e02c94fdbfcf
betegon <miguelbetegongarcia@gmail.com>
2025-09-17 14:17:18 +0200

    window controls

--- packages/electron/src/electron/main/index.ts
+++ packages/electron/src/electron/main/index.ts
@@ -47,6 +47,7 @@ const createWindow = () => {
       sandbox: false,
     },
     backgroundColor: "#1e1b4b",
+    ...(process.platform !== "darwin" ? { titleBarOverlay: true } : {}),
   });
 
   // win.webContents.openDevTools();
