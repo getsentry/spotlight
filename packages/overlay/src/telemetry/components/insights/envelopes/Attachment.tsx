791fce0c16e07a5842c8014c193a63aaf92b3e99
Burak Yigit Kaya <byk@sentry.io>
2025-09-25 15:30:24 +0100

    even more polish, fix memory leaks

--- packages/overlay/src/telemetry/components/insights/envelopes/Attachment.tsx
+++ packages/overlay/src/telemetry/components/insights/envelopes/Attachment.tsx
@@ -1,34 +1,43 @@
 import type { EnvelopeItem } from "@sentry/core";
+import { type ReactNode, useEffect, useMemo } from "react";
+import { ReactComponent as Download } from "~/assets/download.svg";
 import JsonViewer from "../../shared/JsonViewer";
-import type { ReactNode } from "react";
 
 export default function Attachment({ header, attachment }: { header: EnvelopeItem[0]; attachment: string }) {
   let content: ReactNode | null = null;
-  const name = (header.filename as string) || "<unknown>";
+  let extension = "bin";
   if (header.content_type === "text/plain") {
+    extension = "txt";
     content = (
       <pre className="text-primary-300 whitespace-pre-wrap break-words font-mono text-sm p-2 bg-primary-900 rounded-sm">
         {attachment}
       </pre>
     );
   } else if (header.content_type === "application/json") {
+    extension = "json";
     content = <JsonViewer data={JSON.parse(attachment)} />;
-  } else {
-    // we don't know what this is to just provide a download button
-    const downloadUrl = URL.createObjectURL(
-      new Blob([atob(attachment)], { type: (header.content_type as string) || "application/octet-stream" }),
-    );
-    const downloadName = `${name}.bin`;
-    content = (
-      <a href={downloadUrl} download={downloadName}>
-        Download
-      </a>
-    );
   }
+  const name = (header.filename as string) || `untitled-attachment.${extension}`;
+
+  const downloadUrl = useMemo(
+    () =>
+      URL.createObjectURL(
+        new Blob([extension === "bin" ? atob(attachment) : attachment], {
+          type: (header.content_type as string) || "application/octet-stream",
+        }),
+      ),
+    [attachment, header.content_type, extension],
+  );
+  useEffect(() => () => URL.revokeObjectURL(downloadUrl), [downloadUrl]);
 
   return (
     <>
-      <h3>📎 {name}</h3>
+      <h3>
+        📎{" "}
+        <a href={downloadUrl} download={name}>
+          {name} <Download className="w-4 h-4 opacity-60 group-hover:opacity-100 transition-opacity inline" />
+        </a>
+      </h3>
       {content}
     </>
   );
