64992b391cecc93e71c00ba715df138d26113b6e
Burak Yigit Kaya <byk@sentry.io>
2025-09-26 17:58:06 +0100

    fix error

--- packages/overlay/src/index.tsx
+++ packages/overlay/src/index.tsx
@@ -1,7 +1,7 @@
 import fontStyles from "@fontsource/raleway/index.css?inline";
 import { CONTEXT_LINES_ENDPOINT } from "@spotlightjs/sidecar/constants";
 import App from "./App";
-import { DEFAULT_SIDECAR_STREAM_URL, DEFAULT_SIDECAR_URL } from "./constants";
+import { DEFAULT_SIDECAR_STREAM_URL } from "./constants";
 import globalStyles from "./index.css?inline";
 import { Router } from "./lib/Router";
 import { on, trigger } from "./lib/eventTarget";
