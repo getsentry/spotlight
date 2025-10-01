e88da6b47955e1e82da60f80461439bd319e21fa
Aditya Mathur <57684218+MathurAditya724@users.noreply.github.com>
2025-10-01 20:47:33 +0530

    fix: removed the tooltip from the filter dropdown (#979)

--- packages/overlay/src/telemetry/components/shared/FilterDropdown.tsx
+++ packages/overlay/src/telemetry/components/shared/FilterDropdown.tsx
@@ -9,7 +9,6 @@ import {
   DropdownMenuRadioItem,
   DropdownMenuTrigger,
 } from "~/ui/dropdownMenu";
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "~/ui/tooltip";
 
 interface FilterOption {
   label: string;
@@ -19,7 +18,6 @@ interface FilterOption {
 interface FilterDropdownProps {
   icon: ElementType;
   label: string;
-  tooltip: string;
   options: FilterOption[];
   activeFilters: string[];
   onFilterChange: (value: string, checked: boolean) => void;
@@ -29,55 +27,45 @@ interface FilterDropdownProps {
 export function FilterDropdown({
   icon: Icon,
   label,
-  tooltip,
   options,
   activeFilters,
   onFilterChange,
   type,
 }: FilterDropdownProps) {
   return (
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button
-                variant="outline"
-                size="sm"
-                className="border-primary-700 hover:bg-primary-900 bg-primary-950 h-10 mix-blend-screen hover:text-white"
-              >
-                <Icon className="mr-1 h-3 w-3" />
-                {label}
-                <ChevronDown className="ml-1 h-3 w-3" />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="border-primary-700 bg-primary-950 max-h-52 text-white">
-              {type === "checkbox" &&
-                options.map(filter => (
-                  <DropdownMenuCheckboxItem
-                    key={filter.value}
-                    checked={activeFilters.includes(filter.value)}
-                    onCheckedChange={checked => onFilterChange(filter.value, checked)}
-                  >
-                    {filter.label}
-                  </DropdownMenuCheckboxItem>
-                ))}
-              {type === "radio" && (
-                <DropdownMenuRadioGroup value={activeFilters[0]} onValueChange={value => onFilterChange(value, true)}>
-                  {options.map(filter => (
-                    <DropdownMenuRadioItem key={filter.value} value={filter.value}>
-                      {filter.label}
-                    </DropdownMenuRadioItem>
-                  ))}
-                </DropdownMenuRadioGroup>
-              )}
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </TooltipTrigger>
-        <TooltipContent>
-          <p>{tooltip}</p>
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          size="sm"
+          className="border-primary-700 hover:bg-primary-900 bg-primary-950 h-10 mix-blend-screen hover:text-white"
+        >
+          <Icon className="mr-1 h-3 w-3" />
+          {label}
+          <ChevronDown className="ml-1 h-3 w-3" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="border-primary-700 bg-primary-950 max-h-52 text-white">
+        {type === "checkbox" &&
+          options.map(filter => (
+            <DropdownMenuCheckboxItem
+              key={filter.value}
+              checked={activeFilters.includes(filter.value)}
+              onCheckedChange={checked => onFilterChange(filter.value, checked)}
+            >
+              {filter.label}
+            </DropdownMenuCheckboxItem>
+          ))}
+        {type === "radio" && (
+          <DropdownMenuRadioGroup value={activeFilters[0]} onValueChange={value => onFilterChange(value, true)}>
+            {options.map(filter => (
+              <DropdownMenuRadioItem key={filter.value} value={filter.value}>
+                {filter.label}
+              </DropdownMenuRadioItem>
+            ))}
+          </DropdownMenuRadioGroup>
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
