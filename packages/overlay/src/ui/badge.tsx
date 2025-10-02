431833f9d84c898c79c9046c9cb4a50e06db6a77
betegon <miguelbetegongarcia@gmail.com>
2025-10-02 14:12:17 +0200

    fix remove unused color prop

--- packages/overlay/src/ui/badge.tsx
+++ packages/overlay/src/ui/badge.tsx
@@ -25,8 +25,8 @@ const badgeVariants = cva(
 
 export interface BadgeProps extends React.HTMLAttributes<HTMLDivElement>, VariantProps<typeof badgeVariants> {}
 
-function Badge({ className, variant, color, ...props }: BadgeProps) {
-  return <div className={cn(badgeVariants({ variant, color }), className)} {...props} />;
+function Badge({ className, variant, ...props }: BadgeProps) {
+  return <div className={cn(badgeVariants({ variant }), className)} {...props} />;
 }
 
 export { Badge, badgeVariants };
