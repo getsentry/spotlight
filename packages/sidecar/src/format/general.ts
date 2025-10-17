269bc6a46de263fdeaf0dfadd033bfd3f7ac74c3
Burak Yigit Kaya <byk@sentry.io>
2025-10-17 22:10:52 +0100

    some tidy up

--- packages/sidecar/src/format/general.ts
+++ packages/sidecar/src/format/general.ts
@@ -5,20 +5,17 @@ import {
   type SentryLogEvent,
   type ParsedEnvelope,
   type SentryEvent,
+  isTraceEvent,
+  type SentryTransactionEvent,
 } from "~/parser/index.js";
 import { processErrorEvent } from "./errors.js";
 import { formatEventOutput } from "./event.js";
 import { processLogEvent } from "./logs.js";
+import { formatTraceSummary, TraceSummary } from "./traces.js";
 
 export const eventHandlers = {
   error: (payload: ErrorEvent) => formatEventOutput(processErrorEvent(payload)),
-  log: (payload: SentryLogEvent) => {
-    const content: string[] = [];
-    for (const log of payload.items) {
-      content.push(processLogEvent(log));
-    }
-    return content;
-  },
+  log: (payload: SentryLogEvent) => payload.items.map(processLogEvent),
 };
 
 export function formatEnvelope(envelope: ParsedEnvelope["envelope"]): string[] {
@@ -32,6 +29,9 @@ export function formatEnvelope(envelope: ParsedEnvelope["envelope"]): string[] {
       formatted.push(eventHandlers.error(payload as ErrorEvent));
     } else if (type === "log" && isLogEvent(payload as SentryEvent)) {
       formatted.push(...eventHandlers.log(payload as SentryLogEvent));
+    } else if (type === "transaction" && isTraceEvent(payload as SentryEvent)) {
+      // todo: extract main part of `extractTraceEventsFromContainer` as a helper to process
+      //       an already parsed envelope
     }
   }
 
