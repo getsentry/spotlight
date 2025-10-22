432cdb802e13d5f0b2fa08db4e4b9a45e4117ef6
mathuraditya724 <mathuraditya724@gmail.com>
2025-10-22 17:32:13 +0530

    wip

--- packages/sidecar/src/messageBuffer.spec.ts
+++ packages/sidecar/src/messageBuffer.spec.ts
@@ -5,10 +5,20 @@ describe("messageBuffer", () => {
   it("basic usage", async () => {
     const messageBuffer = new MessageBuffer<number>(5);
 
+    for (let i = 0; i < 3; i++) {
+      messageBuffer.put(i);
+    }
+
+    expect(messageBuffer.read()).toEqual([2, 1, 0]);
+  });
+
+  it("should remove old items from buffer when new item is added", async () => {
+    const messageBuffer = new MessageBuffer<number>(5);
+
     for (let i = 0; i < 6; i++) {
       messageBuffer.put(i);
     }
 
-    expect(messageBuffer.read()).toEqual([4, 3, 2, 1, 5]);
+    expect(messageBuffer.read()).toEqual([5, 4, 3, 2, 1]);
   });
 });
