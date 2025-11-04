3b145389294b3a5974aa53f7ba57d05f0bd933d7
Burak Yigit Kaya <byk@sentry.io>
2025-11-04 22:45:25 +0000

    revert debugging change

--- packages/sidecar/src/routes/stream/index.ts
+++ packages/sidecar/src/routes/stream/index.ts
@@ -56,7 +56,6 @@ const router = new Hono<HonoEnv>()
     const buffer = getBuffer();
 
     const envelopeId = ctx.req.param("id");
-    console.log(buffer.read().map(c => c.getParsedEnvelope().envelope[0]));
     const container = buffer.read({ envelopeId });
 
     if (container.length === 0) {
