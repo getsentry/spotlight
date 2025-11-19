f0fd21243d6dadea12a997f48c648d9587527971
Burak Yigit Kaya <byk@sentry.io>
2025-11-19 11:00:38 +0000

    fix double sentry init

--- packages/spotlight/src/bootstrap.ts
+++ packages/spotlight/src/bootstrap.ts
@@ -5,6 +5,10 @@ if (typeof process !== 'undefined' && process.versions?.electron) {
   import('./electron-index.tsx');
 } else {
   // Load web entry point
-  import('./index.tsx').then(m => m._init({ debug: true }));
+  // Initialize Sentry first, then the app
+  Promise.all([import("./ui/lib/instrumentation"), import("./index.tsx")]).then(([instrumentation, index]) => {
+    instrumentation.default(); // Call initSentry()
+    return index._init();
+  });
 }
 
