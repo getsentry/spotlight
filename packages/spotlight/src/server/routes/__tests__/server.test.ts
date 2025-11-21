d4e91bafe7ba7500a6074a6c5bd703ce7b7f9d3c
Burak Yigit Kaya <byk@sentry.io>
2025-11-21 23:53:24 +0000

    fix broken test

--- packages/spotlight/src/server/routes/__tests__/server.test.ts
+++ packages/spotlight/src/server/routes/__tests__/server.test.ts
@@ -72,14 +72,14 @@ describe("encoded envelopes", () => {
   const compressors: Record<string, (buf: Buffer) => Buffer> = {
     gzip: gzipSync,
     deflate: deflateSync,
-    brotli: brotliCompressSync,
+    br: brotliCompressSync,
   };
 
   function testEncodedEnvelope(encoding: string) {
     return async () => {
       const sendResponse = await app.request("/stream", {
         method: "POST",
-        body: compressors[encoding](Buffer.from(JSON.stringify(envelopeReactClientSideError))),
+        body: new Uint8Array(compressors[encoding](Buffer.from(JSON.stringify(envelopeReactClientSideError)))),
         headers: {
           "Content-Type": "application/x-sentry-envelope",
           "Content-Encoding": encoding,
